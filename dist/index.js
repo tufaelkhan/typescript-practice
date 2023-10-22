d9dbec913c1e2907f4ecf2140fd849422ff79018
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-22 10:43:42 -0700

    add enum

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,10 @@
 "use strict";
-let user = [3, "khan"];
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -20,4 +20,10 @@
 // let user: [number, string, boolean, number] = [3, "khan", true, 0]
 // user.push(1)
 
-
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+// PascalCase
+const enum Size { Small = 1, Medium, Large }
+let mySize: Size = Size.Medium;
+console.log(mySize);

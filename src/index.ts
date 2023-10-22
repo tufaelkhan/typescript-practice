a4584a8e37462fa745ed3ac3608d7d9b3e87370d
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-22 10:19:59 -0700

    add type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-let age: number = 23;
-if(age < 50)
-    age += 10;
-console.log(age);
+// let age: number = 23;
+// if(age < 50)
+//     age += 10;
+// console.log(age);
+
+let sales = 24_35_4345;
+let course = "TypeScript";
+let is_published = true;
+let label;
+label = 1;
+label = "khan";
+
+function render(document){
+    console.log(document)
+}
+

4a1ed50f0caf4b0b6f85d7eb0fb1cf14d16ae608
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-22 10:32:17 -0700

    add lookp

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,18 @@
 //     age += 10;
 // console.log(age);
 
-let sales = 24_35_4345;
-let course = "TypeScript";
-let is_published = true;
-let label;
-label = 1;
-label = "khan";
+// let sales = 24_35_4345;
+// let course = "TypeScript";
+// let is_published = true;
+// let label;
+// label = 1;
+// label = "khan";
 
-function render(document){
-    console.log(document)
-}
+// function render(document){
+//     console.log(document)
+// }
 
+// let numbers: number[] = []
+// numbers[0] = 1;
+// numbers.forEach((element => element.toFixed));
+let user: [number, string] = [3, "khan"]

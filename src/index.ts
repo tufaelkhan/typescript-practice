aa0ef9eaf7a04b0aaa0fd4fb56992146b6570fa3
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-22 19:55:49 -0700

    add literal type

--- src/index.ts
+++ src/index.ts
@@ -56,4 +56,22 @@
 // kgTobs(10);
 // kgTobs("30kg")
 
+// type Draggable = {
+//     drag: () => void
+// }
+// type Resizable = {
+//     resize: () => void
+// }
+// type UIWidget = Draggable & Resizable
+// let textBox: UIWidget = {
+//     drag: ()=> {},
+//     resize: ()=> {}
+// }
+
+// Literal exact, specific
+type Quantity = 50 | 100
+let quantity: Quantity = 100;
+
+type Metric = 'cm' | 'inch';
+
 
